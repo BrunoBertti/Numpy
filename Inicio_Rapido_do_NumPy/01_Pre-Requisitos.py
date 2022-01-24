21ff5220903d32845f6e06b4a810333f13b0fc44
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-24 11:29:40 -0300

    Initial commit

---
+++ Inicio_Rapido_do_NumPy/01_Pre-Requisitos.py
@@ -0,0 +1,19 @@
+########## Pré-requisitos ##########
+
+    # Você precisará conhecer um pouco de Python. Para uma atualização, consulte o tutorial do Python.
+
+    # Para trabalhar os exemplos, você precisará do matplotlib instalado além do NumPy.
+
+##### Perfil do aluno
+
+    # Esta é uma visão geral rápida de matrizes no NumPy. Ele demonstra como arrays n-dimensionais (n >= 2) são representados e podem ser manipulados. Em particular, se você não souber como aplicar funções comuns a arrays n-dimensionais (sem usar loops for), ou se quiser entender as propriedades de eixo e forma para arrays n-dimensionais, este artigo pode ser útil.
+
+##### objetivos de aprendizado
+
+    # Após a leitura, você deverá ser capaz de:
+
+        # Entenda a diferença entre arrays unidimensionais, bidimensionais e n-dimensionais no NumPy;
+
+        # Entender como aplicar algumas operações de álgebra linear a arrays n-dimensionais sem usar loops for;
+
+        # Compreender as propriedades de eixo e forma para matrizes n-dimensionais. 
